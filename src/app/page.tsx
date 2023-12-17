d8cbd9039b1b72aeac6fdb1933b4b2c7f803f27b
Antonio Sitoe <antoniositoehl@gmail.com>
2023-12-17 18:18:19 +0200

    ll

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,29 @@
-import Image from "next/image";
 import Link from "next/link";
+import { sql } from "@vercel/postgres";
+
+const Quizzes = async () => {
+  const quizes = await sql`
+  SELECT * FROM quizzes
+  `;
+
+  return (
+    <ul>
+      {quizes.rows.map((item) => {
+        return (
+          <li key={item.id}>
+            <Link href={"/quiz/" + item.id}>Quiz {item.id}</Link>
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
 
 export default function Home() {
   return (
     <main>
       <h1>Quiz</h1>
-      <ul>
-        <li>
-          <Link href="/quiz/1">Quiz 1</Link>
-        </li>
-        <li>
-          <Link href="/quiz/2">Quiz 2</Link>
-        </li>
-        <li>
-          <Link href="/quiz/3">Quiz 3</Link>
-        </li>
-      </ul>
+      <Quizzes />
     </main>
   );
 }
--- src/app/quiz/[id]/page.tsx
+++ src/app/quiz/[id]/page.tsx
@@ -1,8 +1,19 @@
+import { sql } from "@vercel/postgres";
+
 interface Props {
   params: {
     id: string;
   };
 }
-export default function QizPage({ params }: Props) {
+
+async function get({ id }: any) {
+  const quizes = await sql`
+  SELECT name, description FROM quizzes
+WHERE id =${id}`;
+  return quizes;
+}
+export default async function QizPage({ params }: Props) {
+  const a = await get(params.id);
+  console.log(a.oid);
   return <div>quiz {params.id}</div>;
 }
